2f738ef5669ad8172657f26aa0f832fec7ea85ea
Vaidas <vaidas.ruskys@gmail.com>
2019-02-10 10:02:52 +0200

    Compiler pass updated

--- src/Translation/DependencyInjection/Compiler/CreateTranslatorPass.php
+++ src/Translation/DependencyInjection/Compiler/CreateTranslatorPass.php
@@ -6,14 +6,13 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
-use VaidasRuskys\DatabaseTranslator\Translation\Repository\OrmRepository;
 use VaidasRuskys\DatabaseTranslator\Translation\Translator;
 
 class CreateTranslatorPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        $translator = new Definition(Translator::class, [new Reference(OrmRepository::class)]);
+        $translator = new Definition(Translator::class, [new Reference('vaidas_ruskys.translator.orm.repository')]);
         $container->setDefinition('vaidas_ruskys.translator', $translator);
     }
 }
